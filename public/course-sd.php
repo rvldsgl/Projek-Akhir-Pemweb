dc775260db6ac338e33d3f2b88d52e94d346fc84
Hanif Robby <96159381+HanifRobby@users.noreply.github.com>
2023-05-27 14:25:15 +0700

    course sd

---
+++ public/course-sd.php
@@ -0,0 +1,53 @@
+<?php
+
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="web icon" href="../assets/icons/Logo.png">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Course SD</title>
+</head>
+
+<body>
+
+    <!-- header -->
+
+    <header>
+        <?php require '../components/navbar-login.html' ?>
+    </header>
+
+    <!-- main -->
+
+    <main>
+
+        <div class="course-board">
+            <a class="course-card" href="">
+                <div class="course-img">
+                    <img class="course-img" src="../assets/images/equation.png" alt="">
+                </div>
+                <div class="course-head">
+                    <div class="course-title">
+                        <h1>Matematika</h1>
+                    </div>
+                    <div class="course-content">
+                        <p>Matematika adalah cabang ilmu yang mempelajari tentang angka, hitungan, struktur, ruang, dan pola yang bertujuan untuk mengembangkan pemahaman konsep dasar matematika dan keterampilan komputasi</p>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+    </main>
+
+    <!-- footer -->
+
+    <footer>
+        <?php include '../components/footer.html'; ?>
+    </footer>
+</body>
---
+++ public/profile.php
@@ -0,0 +1,70 @@
+<?php
+
+
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="web icon" href="../assets/icons/Logo.png">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Edukasi</title>
+</head>
+
+<body>
+
+    <!-- header -->
+
+    <header>
+        <?php
+        require '../components/navbar-login.html';
+        ?>
+    </header>
+
+    <!-- main -->
+
+    <main>
+
+        <div class="profil">
+            <div class="profile-wrap1">
+                <img class="profile-img" src="../assets/images/user-img.png" alt="">
+            </div>
+            <div class="profile-wrap2">
+                <div class="profile-info">
+                    <div class="profile-head">
+                        <h1>Profile</h1>
+                        <a class="edit-button" href="" style="height:fit-content;">Edit</a>
+                    </div>
+                    <div class="name">
+                        <label for=""><h2>Nama Lengkap</h2></label>
+                    </div>
+                    <div class="school">
+                        <label for=""><h2>Sekolah</h2></label>
+                    </div>
+                    <div class="jenjang">
+                        <label for=""><h2>Jenjang</h2></label>
+                    </div>
+                </div>
+                <div class="profile-opt">
+                    <a class="premium-button" href="">Get Premium Plan</a>
+                    <a class="logout-button" href="">Log Out</a>
+                </div>
+            </div>
+        </div>
+        
+    </main>
+
+    <!-- footer -->
+
+    <footer>
+        <?php
+        require '../components/footer.html';
+        ?>
+    </footer>
+
+
+
+</body>
