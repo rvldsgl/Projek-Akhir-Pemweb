c86574644b57b5c4cedc38591256c4ed3943c50d
Revaldo Gemino Kantana Sagala <aldogksagala@gmail.com>
2023-05-27 23:41:37 +0700

    Create register.php

---
+++ public/register.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="UTF-8">
+        <link rel="web icon" href="../assets/icons/Logo.png">
+        <link rel="stylesheet" href="../css/register.css">
+        <title>REGISTER</title>
+    </head>
+    <body>
+        <div class="login">
+            <div class="group1">
+                <img class="manusia" src="../assets/images/laptopan.png" alt="human">
+                <h1 ><a class="title" href="../index.html">EDUKASI</a></h1>
+            </div>
+            <div class="group2">
+                <h1 class="judul">REGISTER</h1>
+                <form action="">
+                    <label class="email" for="username">Fullname:</label>
+                    <br>
+                    <input class="kolom" type="text" id="namaPanjang" name="username" required>
+                    <br>
+                    <br>
+                    <label class="password" for="password">Email:</label>
+                    <br>
+                    <input class="kolom" type="text" id="email" name="email" required>
+                    <br>
+                    <br>
+                    <label class="email" for="username">Password:</label>
+                    <br>
+                    <input class="kolom" type="password" id="password" name="password" required>
+                    <br>
+                    <br>
+                    <label class="password" for="password">Nomor Handphone:</label>
+                    <br>
+                    <input class="kolom" type="text" id="noHP" name="password" required>
+                    <br>
+                    <br>
+                    <input class="kotak" type="checkbox" id="myCheckbox" name="myCheckbox">
+                    <label class="cek" for="myCheckbox">Saya sudah membaca syarat dan ketentuan</label>
+                    <br>
+                    <br>
+                    <input class="tombol" type="submit" value="Register">
+                    <br>
+                        <div class="kata">
+                            <p style="font-weight: 200;">Already Registerd? <a class="link" href="login.html">Login</a></p>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
